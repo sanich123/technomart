a47627881e2b85768b4df0e94125246c4bb86bc9
Alexander <aovoronin.piano@gmail.com>
2021-03-30 22:32:06 +0300

    Немного припилил js

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,63 @@
-const sliderButtons = document.querySelectorAll('.slider-controls-button');
-const currentButton = document.querySelector('.current');
+/*const servicesButtons = document.querySelectorAll('.services-button');
 
-console.log(sliderButton);
+for (let servicesButton of servicesButtons) {
+  console.log(servicesButton);
+  servicesButton.addEventListener('click', function(evt){
+    evt.preventDefault();
+    servicesButton.classList.add('current');
+  })
+};
+*/
+const button1 = document.querySelector('.services-button:nth-child(1)');
+const button2 = document.querySelector('.services-button:nth-child(2)');
+const button3 = document.querySelector('.services-button:nth-child(3)');
+const delivery = document.querySelector('.service-list-item:nth-child(1)');
+const garanty = document.querySelector('.service-list-item:nth-child(2)');
+const credit = document.querySelector('.service-list-item:nth-child(3)');
+console.log(garanty);
 
-for (let sliderButton of sliderButtons) {
-  sliderButton.addEventListener('click', function(evt){
+button2.addEventListener('click', function(evt){
   evt.preventDefault();
-  sliderButton.classList.add('current');}
+  garanty.classList.remove('visually-hidden');
+  delivery.classList.add('visually-hidden');
+  credit.classList.add('visually-hidden');
+  button2.classList.add('current');
+  button1.classList.remove('current');
+  button3.classList.remove('current');
 });
 
+button3.addEventListener('click', function(evt){
+  evt.preventDefault();
+  credit.classList.remove('visually-hidden');
+  garanty.classList.add('visually-hidden');
+  delivery.classList.add('visually-hidden');
+  button3.classList.add('current');
+  button2.classList.remove('current');
+  button1.classList.remove('current');
+});
+
+button1.addEventListener('click', function(evt){
+  evt.preventDefault();
+  delivery.classList.remove('visually-hidden');
+  credit.classList.add('visually-hidden');
+  garanty.classList.add('visually-hidden');
+  button1.classList.add('current');
+  button3.classList.remove('current');
+  button2.classList.remove('current');
+});
+
+
+const slideForward = document.querySelector('.slider-button-2');
+const slideBack = document.querySelector('.slider-button-1');
+const slidePerforator = document.querySelector('.perforator');
+const slideDrill = document.querySelector('.drill');
+
+slideForward.addEventListener('click', function(evt){
+slidePerforator.classList.remove('slide-current');
+slideDrill.classList.add('slide-current');
+});
+
+slideBack.addEventListener('click', function(evt){
+  slidePerforator.classList.add('slide-current');
+  slideDrill.classList.remove('slide-current');
+  });
